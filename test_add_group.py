1616edf099d08df0ec18ea9775dfc8c1bf0c79a4
Vasili9 <olgalanczuk@gmail.com>
2020-12-09 15:41:32 -0500

    methods create

--- test_add_group.py
+++ test_add_group.py
@@ -11,15 +11,15 @@ def is_alert_present(wd):
         return False
 
 class test_add_group(unittest.TestCase):
+
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
 
-    def test_add_group(self):
-        wd = self.wd
-        # open home page
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
-        #login
+
+    def login(self, wd):
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -27,8 +27,11 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
-        # open groups page
+
+    def open_groups_page(self, wd):
         wd.find_element_by_link_text("groups").click()
+
+    def create_group(self, wd):
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group firm
@@ -43,11 +46,21 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys("work")
         # submit group creation
         wd.find_element_by_name("submit").click()
-        # return to groups page
+
+    def retern_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
-        # logout
+
+    def logout(self, wd):
         wd.find_element_by_link_text("Logout").click()
-        
+
+    def test_add_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.retern_to_groups_page(wd)
+        self.logout(wd)
 
     def tearDown(self):
         self.wd.quit()
