e19178671c99256da264902ae188848693d094c9
Vasili9 <olgalanczuk@gmail.com>
2020-12-09 14:21:39 -0500

    add comment

--- test_add_group.py
+++ test_add_group.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 from selenium.webdriver.firefox.webdriver import WebDriver
 import unittest
 
@@ -19,7 +18,9 @@ class test_add_group(unittest.TestCase):
     def test_add_group(self):
         success = True
         wd = self.wd
+        # open home page
         wd.get("http://localhost/addressbook/")
+        #login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -27,8 +28,11 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
         wd.find_element_by_xpath("//input[@value='Login']").click()
+        # open groups page
         wd.find_element_by_link_text("groups").click()
+        # init group creation
         wd.find_element_by_name("new").click()
+        # fill group firm
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("new_group")
@@ -38,8 +42,11 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("work")
+        # submit group creation
         wd.find_element_by_name("submit").click()
+        # return to groups page
         wd.find_element_by_link_text("group page").click()
+        # logout
         wd.find_element_by_link_text("Logout").click()
         self.assertTrue(success)
 
