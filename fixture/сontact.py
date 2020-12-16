52d22963b1a6cbffa377a608e8db4fd94e340071
Vasili9 <olgalanczuk@gmail.com>
2020-12-16 11:18:25 -0500

    Zadanie 5 (for group and contact). Helper Class. Manager Class.

---
+++ fixture/сontact.py
@@ -0,0 +1,31 @@
+
+class ContactHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def open_contact_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("add new").click()
+
+    def create(self, contact):
+        wd = self.app.wd
+        self.open_contact_page()
+        # init contact creation
+        wd.find_element_by_link_text("add new").click()
+        # fill contact form
+        wd.find_element_by_name("firstname").click()
+        wd.find_element_by_name("firstname").clear()
+        wd.find_element_by_name("firstname").send_keys(contact.name)
+        wd.find_element_by_name("lastname").click()
+        wd.find_element_by_name("lastname").clear()
+        wd.find_element_by_name("lastname").send_keys(contact.last)
+        wd.find_element_by_name("address").click()
+        wd.find_element_by_name("address").clear()
+        wd.find_element_by_name("address").send_keys(contact.address)
+        # submit group creation
+        wd.find_element_by_name("submit").click()
+        self.retern_to_home_page()
+
+    def retern_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
