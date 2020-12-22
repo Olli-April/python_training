dd6a01db1930305331245d411901052d1ae8e9be
Vasili9 <olgalanczuk@gmail.com>
2020-12-22 10:21:33 -0500

    добавлена валидация фикстуры перед каждым тестом.

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -2,6 +2,4 @@
 from model.contact import Contact
 
 def test_add_new_contact(app):
-        app.session.login(username="admin", password="secret")
         app.contact.create(Contact(first_name="Lili", last="And", address="New York"))
-        app.session.logout()
--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -1,6 +1,4 @@
 from model.group import Group
 
 def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
     app.group.edit_first_group(Group(name="Group_Edit", header="Edit_header", footer="Edit_footer"))
-    app.session.logout()
