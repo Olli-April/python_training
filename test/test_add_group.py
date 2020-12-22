836e73ce5fab48294b35a1d3b782cc172a52d7e0
Vasili9 <olgalanczuk@gmail.com>
2020-12-22 15:57:28 -0500

    Zadanie 8

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -4,6 +4,5 @@ from model.group import Group
 def test_add_group(app):
     app.group.create(Group(name="new_group", header="pro", footer="work"))
 
-
 def test_add_empty_group(app):
-    app.group.create(Group(name="", header="", footer=""))
+   app.group.create(Group(name="", header="", footer=""))
