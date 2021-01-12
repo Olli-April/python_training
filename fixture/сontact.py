4a1e695a282375571a377b2ede7909d815e503f0
Vasili9 <olgalanczuk@gmail.com>
2021-01-11 20:35:56 -0500

    Zadanie 11 (внесены измения в фикстуру контакта: method_open_contact; return_to_home_page;

--- fixture/сontact.py
+++ fixture/сontact.py
@@ -6,7 +6,13 @@ class ContactHelper:
 
     def open_contact_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("add new").click()
+        if not (wd.current_url.endswith("/addressbook/") and
+                len(wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
+            wd.find_element_by_link_text("home").click()
+
+    def return_to_home_page(self):
+        wd = self.app.wd
+        wd.find_element_by_link_text("home page").click()
 
     def create(self, contact):
         wd = self.app.wd
@@ -35,6 +41,7 @@ class ContactHelper:
 
     def modify_first_contact(self, new_contact_data):
         wd = self.app.wd
+        self.open_contact_page()
         wd.find_element_by_xpath("//img[@alt='Edit']").click()
         # edit contact form
         self.fill_form_contact(new_contact_data)
@@ -45,23 +52,17 @@ class ContactHelper:
 
     def delete_first_contact(self):
         wd = self.app.wd
+        self.open_contact_page()
         wd.find_element_by_name("selected[]").click()
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to_alert().accept()
         wd.find_elements_by_css_selector("div.msgbox")
-        self.return_to_home_page()
+        self.open_contact_page()
         self.contact_cache = None
-
-    def return_to_home_page(self):
-        wd = self.app.wd
-       # if not (wd.current_url.endswith("/addressbook/") and
-        #        len(wd.find_elements_by_xpath("//input[@value='Send e-Mail']")) > 0):
-        wd.find_element_by_link_text("home").click()
-
 
     def count(self):
         wd = self.app.wd
-        self.return_to_home_page()
+        self.open_contact_page()
         return len(wd.find_elements_by_name("selected[]"))
 
     contact_cache = None
@@ -69,7 +70,7 @@ class ContactHelper:
     def get_contact_list(self):
         if self.contact_cache is None:
             wd = self.app.wd
-            self.return_to_home_page()
+            self.open_contact_page()
             self.contact_cache = []
             for element in wd.find_elements_by_name("entry"):
                 cells = element.find_elements_by_tag_name("td")
