d13c66dae79810a554fd4e159efef7d8bf1177db
Vasili9 <olgalanczuk@gmail.com>
2021-01-13 23:16:15 -0500

    Задание 14  (Реализ. проверки для полей контакта на главной странице)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,8 +3,8 @@ from model.contact import Contact
 
 def test_add_new_contact(app):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Lili", lastname="And", address="New York", homephone="1111111",
-                      workphone="22222", mobilephone="33333", secondaryphone="44444")
+    contact = Contact(firstname="Lili", lastname="And", address="New York", homephone="11111",
+                      mobilephone="33333", workphone="22222", secondaryphone="44444")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
