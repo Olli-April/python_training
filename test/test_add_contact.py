f5c4730fd0dcd7947105a98f4551a041636f7d31
Vasili9 <olgalanczuk@gmail.com>
2021-01-14 12:09:29 -0500

    Параметризация тестов создания групп и контактов(черновик)

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,12 +1,29 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
-def test_add_new_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", middlename="", lastname="", mobilephone="")] + [
+    Contact(firstname=random_string("firstname", 15), middlename=random_string("middlename", 15),
+            lastname=random_string("lastname", 15), mobilephone=random_string("7", 10))
+    for i in range(5)]
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Lili", lastname="And", address="New York", homephone="11111",
-                      mobilephone="33333", workphone="22222", secondaryphone="44444")
     app.contact.create(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+
+#address=random_string("address", 15), middlename=random_string("middlename", 20),
+# address2=random_string("address2", 10), email=random_string("email", 10),
+# email2=random_string("email2", 10), email3=random_string("email3", 10), homephone=random_string("homephone", 10),
+# workphone = random_string("workphone", 10), secondaryphone = random_string("secondaryphone", 10)
