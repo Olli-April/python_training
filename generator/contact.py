24c70819ccb277d9f269ba99faf4146807af033c
Vasili9 <olgalanczuk@gmail.com>
2021-01-20 11:48:47 -0500

    Генерация тестовых данных в формате JSON (контакты)

---
+++ generator/contact.py
@@ -0,0 +1,37 @@
+from model.contact import Contact
+import random
+import string
+import jsonpickle
+import os.path
+import getopt
+import sys
+import json
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "n:f", ["number of contacts", "file"])
+except getopt.GetoptError as err:
+    getopt.usage()
+    sys.exit(2)
+
+n = 5
+f = "data/contacts.json"
+
+for o, a in opts:
+    if o == "-n":
+        n = int(a)
+    elif o == "-f":
+        f = a
+
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [Contact(firstname="", middlename="", lastname="", mobilephone="")] + [
+    Contact(firstname=random_string("firstname", 15), middlename=random_string("middlename", 15),
+            lastname=random_string("lastname", 15), mobilephone=random_string("7", 10))
+    for i in range(5)]
+
+file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", f)
+
+with open(file, "w") as out:
+    out.write(json.dumps(testdata, default=lambda x: x.__dict__, indent=2))
